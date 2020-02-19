e95ddff439ff4c8aa833513faaab49108d1afa8b
Luke McKinley <luke.mckinley21@gmail.com>
2020-02-19 11:26:11 -0600

    updated codes

--- 4_extra_practice.js
+++ 4_extra_practice.js
@@ -2,5 +2,16 @@
 // If you have time, try coming up with a different approach/solution for reversing an array
 
 // YOUR CODE HERE
+var arr = [1,2,3,4];
+var newArr=[];
+
+var temp =arr[0];
+var temp2=arr[1];
+var temp3=arr[2];
+var temp4=arr[3];
+
+newArr=[temp4,temp3,temp2,temp];
+
+console.log(newArr);
 
 
